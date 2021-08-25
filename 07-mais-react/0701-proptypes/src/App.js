560d3718d0b7f7bfc9d21e0dafa0f4a09a63e2f8
Robson <robsonshockwavegithub@gmail.com>
2021-08-24 23:34:14 -0300

    proptypes, lazy/suspense, memo, useReducer, classes finalizados

---
+++ 07-mais-react/0701-proptypes/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button';
+
+const App = () => {
+  return (
+    <div>
+      <Button margin="20px" width={300}>
+        Clique Aqui
+      </Button>
+    </div>
+  );
+};
+
+export default App;
---
+++ 07-mais-react/0704-usereducer/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Exemplo from './Exemplo';
+
+function reducer(state, action) {
+  switch (action) {
+    case 'aumentar':
+      return state + 1;
+    case 'diminuir':
+      return state - 1;
+    default:
+      throw new Error();
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = React.useReducer(reducer, 0);
+
+  return (
+    <div>
+      <button onClick={() => dispatch('aumentar')}>+</button>
+      <button onClick={() => dispatch('diminuir')}>-</button>
+      <p>{state}</p>
+      <Exemplo />
+    </div>
+  );
+};
+
+export default App;
